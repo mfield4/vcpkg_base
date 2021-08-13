b794b4766872b932f41bc4850a7b9fd2e319c9a4
mfield4 <michaelcfield97@gmail.com>
2021-08-12 18:16:30 -0700

    Remove vcpkg submodule

--- cmake/VcpkgSetup.cmake
+++ cmake/VcpkgSetup.cmake
@@ -1,4 +1,12 @@
-set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake CACHE STRING "Vcpkg toolchain file")
+
+include(FetchContent)
+
+FetchContent_Declare(vcpkg GIT_REPOSITORY https://github.com/microsoft/vcpkg.git)
+FetchContent_MakeAvailable(vcpkg)
+set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
+
+
+#set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake CACHE STRING "Vcpkg toolchain file")
 
 option(BUILD_TESTS "Build tests" ON)
 if (BUILD_TESTS)
